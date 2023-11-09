27840d63e62ce71f7a89a26953384c613c5e5d66
theubaidahmed <theubaidahmed@gmail.com>
2023-11-09 13:03:47 +0530

    Initialize template 02

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import './utilities/axios';
+
+//pages
+import Header from './components/Header';
+import Home from './pages/Home';
+
+const App = () => {
+    return (
+        <Header>
+            <Routes>
+                <Route path='/' element={<Home />} />
+            </Routes>
+        </Header>
+    );
+};
+
+export default App;
