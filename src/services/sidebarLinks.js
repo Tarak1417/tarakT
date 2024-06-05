059d1af89c85586b0b486e3757bd812909a36858
Yogesh Pawar <yogeshpawar1118@gmail.com>
2024-06-06 01:56:13 +0530

    fix  add

--- src/services/sidebarLinks.js
+++ src/services/sidebarLinks.js
@@ -110,27 +110,27 @@ const menuItems = [
             {
                 label: 'Dashboard',
                 icon: <Attend />,
-                to: '/dashboardproject',
+                to: '/employees/award',
             },
             {
                 label: 'Project List',
                 icon: <Leave/>,
-                to: '/OverViewCalender',
+                to: '/employees/award',
             },
             {
                 label: 'View Project',
                 icon: <Download/>,
-                to: '/ViewProject',
+                to: '/employees/award',
             },
             {
                 label: 'Overview Calendar',
                 icon: <Person/>,
-                to: '/OverTime',
+                to: '/employees/award',
             },
             {
                 label: 'New Project',
                 icon: <Person/>,
-                to: '/newProject',
+                to: '/employees/award',
             },
            
         ],
