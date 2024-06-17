b2ede3cd7755578b53c1001f06c8b3bd4913a794
Yogesh Pawar <yogeshpawar1118@gmail.com>
2024-06-17 15:52:14 +0530

    fix code marge

--- src/pages/Organization/ListOrganization.jsx
+++ src/pages/Organization/ListOrganization.jsx
@@ -25,61 +25,93 @@ import {
 import DeleteIcon from "@mui/icons-material/Delete";
 import EditIcon from "@mui/icons-material/Edit";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 // Tabs Section
 const ListOrganization = () => {
   const navigate = useNavigate();
   const [organizations, setOrganization] = useState([{ ide: 1 }]);
 
-  function handleDelete() {
+  async function handleDelete(org) {
+    try {
+      const response = await axios.delete(`/hr/organization/${org._id}`);
+
+      let data = response.data;
+      if (data.success) {
+        getOrganizations();
+        // setShowMessage({
+        //   show: true,
+        //   message: "Subscribe Successfully",
+        //   severity: "success",
+        // });
+      }
+    } catch (e) {
+      console.log("Payment method created:", e);
+      // setShowMessage({
+      //   show: true,
+      //   message: "Error creating Subscriptions ",
+      //   severity: "error",
+      // });
+    }
     return { name: "" };
   }
 
-  function handleEdit() {
+  function handleEdit(org) {
     return { name: "" };
   }
 
-  function handleSelect() {
-    navigate("/");
+  async function handleSelect(org) {
+    if (org.status) {
+      try {
+        const response = await axios.post(`/hr/organization/select`, {
+          organizationId: org._id,
+        });
+        let data = response.data;
+        if (data.success) {
+          localStorage.setItem("org", JSON.stringify(org));
+          navigate("/");
+        }
+      } catch (e) {
+        console.log("Error select of Organization", e);
+      }
+    }
   }
 
-  const getColor = (status) => {
-    switch (status) {
-      case "Active":
-        return "bg-green-300 text-green-700";
-      case "In-active":
-        return "bg-red-300 text-red-700";
-      default:
-        return "bg-gray-300 text-gray-700";
+  const getColor = (status = false) => {
+    if (status) {
+      return "bg-green-300 text-green-700";
+    } else {
+      return "bg-red-300 text-red-700";
     }
   };
 
-   useEffect(()=>{
+  useEffect(() => {
     getOrganizations();
-   },[])
+  }, []);
 
-   const getOrganizations = async ()=>{
-    let organizationName  = localStorage.getItem("tempOrganization") ?? "N/A";
-    setOrganization(  [{ name : organizationName , state : "Active" } ])
-   }
-
-  function createData(name, calories, fat, carbs, protein) {
-    return { name, calories, fat, carbs, protein };
-  }
-
-  const rows = [createData("test", "Active", "edit ")];
+  const getOrganizations = async () => {
+    try {
+      const response = await axios.get(`/hr/organization`);
+      let data = response.data;
+      if (data.success) {
+        setOrganization(data.data);
+      }
+    } catch (e) {
+      console.log("Error List of Organization", e);
+    }
+  };
 
   return (
     <Box
       className="h-screen"
       sx={{
         backgroundColor: "background.main",
-        paddingX: { xs : 3,sm : 5 },
+        paddingX: { xs: 3, sm: 5 },
         paddingY: 10,
         width: "100vw",
         minHeight: "100vh",
       }}
     >
-      <Box sx={{  paddingRight:  { xs : 0  ,  sm : 25 }  }}>
+      <Box sx={{ paddingRight: { xs: 0, sm: 25 } }}>
         <Typography variant="h4">Organization List</Typography>
         <Typography sx={{ marginTop: ".7rem", color: "text.three" }}>
           HR organization refers to the style of coordination, communication and
@@ -93,43 +125,48 @@ const ListOrganization = () => {
           variant="h4"
           sx={{ marginTop: ".2rem", marginBottom: 5, color: "text.three" }}
         >
-          {rows.length}
+          {organizations.length}
         </Typography>
       </Box>
 
-      <Box className="px-2" sx={ {   overflowX: 'auto'} }  >
-      <Grid container sx={{ p: 1 , minWidth : 525  }}>
-        <Grid item  xs={8}>
-          Organization
-        </Grid>
-        <Grid item  xs={2}>
-          Status
-        </Grid>
-        <Grid item  xs={2}>
-          Action
-        </Grid>
-      </Grid>
-      <Divider sx={{  minWidth : 525  }} />
-      <Box>
-        {organizations.map((org, index) => (
-          <Grid container sx={{ p: 1 , minWidth : 525  }}>
-            <Grid item  xs={8} >
-              <div onClick={handleSelect} >
-              {org.name}
-              </div>
-            </Grid>
-            <Grid item  xs={2} >
-              <div className={`px-3 py-1 rounded-lg w-fit ${getColor(org.state)} `}  >
-              {org.state}
-              </div>
-            </Grid>
-            <Grid item  xs={2} >
-              <EditIcon  sx={{ color: "blue", marginRight: 1.5 }} onClick={handleEdit} />
-              <DeleteIcon color="error" onClick={handleDelete} />
-            </Grid>
+      <Box className="px-2" sx={{ overflowX: "auto" }}>
+        <Grid container sx={{ p: 1, minWidth: 525 }}>
+          <Grid item xs={8}>
+            Organization
           </Grid>
-        ))}
-      </Box>
+          <Grid item xs={2}>
+            Status
+          </Grid>
+          <Grid item xs={2}>
+            Action
+          </Grid>
+        </Grid>
+        <Divider sx={{ minWidth: 525 }} />
+        <Box>
+          {organizations.map((org, index) => (
+            <Grid container sx={{ p: 1, minWidth: 525 }}>
+              <Grid item xs={8}>
+                <div onClick={() => handleSelect(org)}>{org.name}</div>
+              </Grid>
+              <Grid item xs={2}>
+                <div
+                  className={`px-3 py-1 rounded-lg w-fit ${getColor(
+                    org.status
+                  )} `}
+                >
+                  {org.status ? "Active" : "In-active"}
+                </div>
+              </Grid>
+              <Grid item xs={2}>
+                <EditIcon
+                  sx={{ color: "blue", marginRight: 1.5 }}
+                  onClick={() => handleEdit(org)}
+                />
+                <DeleteIcon color="error" onClick={() => handleDelete(org)} />
+              </Grid>
+            </Grid>
+          ))}
+        </Box>
       </Box>
     </Box>
   );
