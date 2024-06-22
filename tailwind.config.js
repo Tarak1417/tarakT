6f7169ed08ede85d4f6c267d6472f1f766e270fc
Yogesh Pawar <yogeshpawar1118@gmail.com>
2024-06-22 15:14:42 +0530

    added no scroll bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,5 +6,23 @@ export default {
     extend: {},
   },
 
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }) {
+      addUtilities({
+        ".no-scrollbar::-webkit-scrollbar": {
+          display: "none",
+        },
+        ".no-scrollbar": {
+          "-ms-overflow-style": "none" /* IE and Edge */,
+          "scrollbar-width": "none" /* Firefox */,
+        },
+        ".line-clamp-1": {
+          overflow: "hidden",
+          display: "-webkit-box",
+          "-webkit-box-orient": "vertical",
+          "-webkit-line-clamp": "1",
+        },
+      });
+    },
+  ]
 };
