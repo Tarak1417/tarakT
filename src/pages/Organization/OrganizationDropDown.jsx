54a695c8686de25594359b2031af52a635a95c98
Yogesh Pawar <yogeshpawar1118@gmail.com>
2024-07-06 23:48:55 +0530

    org list added

--- src/pages/Organization/OrganizationDropDown.jsx
+++ src/pages/Organization/OrganizationDropDown.jsx
@@ -1,9 +1,26 @@
 import axios from "axios";
+import Box from '@mui/material/Box';
+
 import React, { useEffect, useState } from "react";
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Divider from '@mui/material/Divider';
+import InboxIcon from '@mui/icons-material/Inbox';
+import DraftsIcon from '@mui/icons-material/Drafts';
+import TaskAltIcon from '@mui/icons-material/TaskAlt';
+import AddCircleOutlineOutlinedIcon from '@mui/icons-material/AddCircleOutlineOutlined';
 
 const OrganizationDropDown = () => {
   const [selectedValue, setSelectedValue] = useState("");
   const [organizations, setOrganization] = useState([]);
+  const [isListVisible, setIsListVisible] = useState(false);
+
+  const toggleListVisibility = () => {
+    setIsListVisible(!isListVisible);
+  };
 
   useEffect(() => {
     getOrganizations();
@@ -51,23 +68,16 @@ const OrganizationDropDown = () => {
     }
   }
   return (
+    <div>
+
+
     <div className="relative flex justify-between">
-      <select
-        className="appearance-none bg-transparent text-xl  text-gray-500 w-full focus:outline-none focus:outline-none  focus:border-none  border-none"
-        value={selectedValue}
-        onChange={handleChange}
-      >
-        {organizations.map((item, index) => (
-          <option
-            key={index}
-            className=" bg-transparent mx-2 text-base text-gray-400"
-            value={item._id}
-          >
-            {item.name}
-          </option>
-        ))}
-      </select>
-      <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+
+    <div className="bg-transparent text-xl text-gray-500 w-full "    onClick={toggleListVisibility}>
+      {selectedValue}
+    </div>
+
+    <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
         <svg
           className="w-4 h-4"
           fill="none"
@@ -82,7 +92,56 @@ const OrganizationDropDown = () => {
             d="M19 9l-7 7-7-7"
           ></path>
         </svg>
+    </div>
+    </div>
+    <div className="relative">
+    <div
+        className={`absolute transition-opacity duration-400 w-full ${
+          isListVisible ? 'opacity-100 z-50' : 'opacity-0 z-0'
+        }`}
+        style={{ background: 'white' }}
+      >
+        {isListVisible && (
+                 <Box sx={{ width: '100%', maxWidth: 360 , backgroundColor: 'background.main'  ,  border: 1, borderColor: '#e7e3e3' }}    className="rounded-b-lg shadow-lg">
+    
+                 <List>
+                   <ListItem disablePadding>
+                     <ListItemButton>
+                     <ListItemIcon><div className="w-4 h-4 bg-red-500 rounded-full ml-1.5 mr-5">   </div>  </ListItemIcon>
+                       <ListItemText primary="Inbox" />
+                       <TaskAltIcon />  
+                     </ListItemButton>
+                   </ListItem>
+                   <Divider />
+                   <ListItem disablePadding>
+                     <ListItemButton >
+                     <ListItemIcon> <AddCircleOutlineOutlinedIcon />  </ListItemIcon>
+                       <ListItemText primary="Add " />
+                     </ListItemButton>
+                   </ListItem>
+                 </List>
+         
+             </Box>
+              
+        )}
       </div>
+    </div>
+
+      {/* <select
+        className="appearance-none bg-transparent text-xl  text-gray-500 w-full focus:outline-none focus:outline-none  focus:border-none  border-none"
+        value={selectedValue}
+        onChange={handleChange}
+      >
+        {organizations.map((item, index) => (
+          <option
+            key={index}
+            className=" bg-transparent mx-2 text-base text-gray-400"
+            value={item._id}
+          >
+            {item.name}
+          </option>
+        ))}
+      </select> */}
     </div>
   );
 };
