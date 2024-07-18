ab7be706b75235efa11c967f413c8335f523a096
Yogesh Pawar <yogeshpawar1118@gmail.com>
2024-07-19 00:37:41 +0530

    fix career pag

--- src/careers/components/brand.jsx
+++ src/careers/components/brand.jsx
@@ -16,7 +16,7 @@ const Brand = ({ shouldRenderMenuIcon=false ,  orgName=""}) => {
     <div className='flex items-center flex-grow gap-2 lg:pl-4'>
       {
         shouldRenderMenuIcon &&
-        <Link to={"https://careers.clikkle.com/"}>
+        <Link to={`/career/${orgName}`}>
         <img
           className='w-[35px] h-[35px] sm:w-[35px] sm:h-[35px]'
           src='https://cdn.clikkle.com/images/clikkle/logo/2023/clikkle.png'
@@ -24,26 +24,24 @@ const Brand = ({ shouldRenderMenuIcon=false ,  orgName=""}) => {
         </Link>
       }
      {
-      shouldRenderMenuIcon &&
+      shouldRenderMenuIcon ?
     <>
-      <Link to={"https://careers.clikkle.com/"}>
+      <Link to={`/career/${orgName}`}>
         <h1 className='text-black dark:text-white text-base sm:text-lg md:text-2xl sm:text-sm font-bold'>
           Clikkle
         </h1>
         </Link>
-        <Link to={"https://careers.clikkle.com/"}>
+        <Link to={`/career/${orgName}`}>
         <h2 className='text-black dark:text-white text-base sm:text-lg md:text-2xl sm:text-xs' >
           Careers
         </h2>
       </Link>
-    </>
-  }
-  {
-    !shouldRenderMenuIcon && 
-    <h1 className='text-black dark:text-white text-base sm:text-lg md:text-2xl sm:text-sm font-bold'>
+    </> : 
+    <Link to={`/career/${orgName}`}>
+     <h1 className='text-black dark:text-white text-base sm:text-lg md:text-2xl sm:text-sm font-bold'>
       {orgName}
     </h1>
-  }  
+    </Link>}  
     </div>
   );
 };
