adfd04e430f03ffbe37213333b988d63304195f2
Tarak1417 <achantatarak@gmail.com>
2024-12-02 17:14:06 +0530

    1.changes done on the app.js and nabar mysapaceOverview,deptpage employeepage Listorganization

--- src/components/Dashboard/MySpace/Overview/ProfilePreview.jsx
+++ src/components/Dashboard/MySpace/Overview/ProfilePreview.jsx
@@ -1,16 +1,46 @@
 import React from "react";
-import { Button, Avatar } from "@mui/material";
+import { Button, Avatar, Typography, IconButton } from "@mui/material";
+import { useUser } from "../../../../hooks/Authorize";
+import { Height } from "@mui/icons-material";
+
 
 export default function ProfilePreview() {
+  const platformUser = useUser();
   return (
     <div className="w-full flex gap-3 p-3 flex-col justify-center items-center ">
       <div className="flex flex-col justify-center items-center">
-        <Avatar
-          alt="Steward Graham"
-          sx={{ height: "7rem", width: "7rem" }}
-          src="/static/images/avatar/1.jpg"
-        />
-        <h1>Steward Graham</h1>
+      <div
+    style={{
+        display: "flex", // Enables flexbox
+        alignItems: "center", // Vertically centers the content
+        justifyContent: "center", // Horizontally centers the content
+        borderWidth: "2px",
+        borderStyle: "solid",
+        borderColor: "blue",
+        width: "200px",
+        height: "200px",
+        borderRadius: "50%", // Creates a perfect circle
+    }}
+>
+    <Typography
+        variant="subtitle1"
+        component="div"
+        fontWeight={600}
+        sx={{
+            whiteSpace: "nowrap",
+            overflow: "hidden",
+            textOverflow: "ellipsis",
+        }}
+    >
+        {platformUser && platformUser.firstName && platformUser.lastName
+            ? `${platformUser.firstName.charAt(0).toUpperCase()}${platformUser.lastName.charAt(0).toUpperCase()}`
+            : "?"}
+    </Typography>
+</div>
+
+
+
+        <h1>{platformUser.lastName}</h1>
         <h1>CEO</h1>
       </div>
       <div className="flex flex-col justify-center items-center">
